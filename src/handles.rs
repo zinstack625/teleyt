6291eccd5075d3ca6c10571018ea054c0f105175
zinstack625 <klimanov625@outlook.com>
2022-03-23 02:31:06 +0300

    limit filesize and switch to yt-dlp

--- src/handles.rs
+++ src/handles.rs
@@ -3,7 +3,7 @@ use telegram_bot::prelude::*;
 use telegram_bot::*;
 
 fn get_name(url: &str) -> Result<String, std::io::Error> {
-    let name = Command::new("youtube-dl")
+    let name = Command::new("yt-dlp")
         .args(["--skip-download", "--get-title", &url])
         .output()?
         .stdout;
@@ -20,8 +20,8 @@ pub async fn vid_handle(api: Api, message: Message) -> Result<(), Error> {
     let link = &message.text().unwrap()[5..];
     let mut vid_name = get_name(&link).unwrap_or_else(|_| { "unknown_name".to_string() });
     vid_name.push_str(".mp4");
-    let vid = Command::new("youtube-dl")
-        .args(["-o", "-", &link])
+    let vid = Command::new("yt-dlp")
+        .args(["-f", "[filesize_approx<=50m]", "-o", "-", &link])
         .output();
     if let Ok(vid) = vid {
         if vid.stdout.is_empty() {
@@ -43,8 +43,8 @@ pub async fn mus_handle(api: Api, message: Message) -> Result<(), Error> {
     let link = &message.text().unwrap()[5..];
     let mut vid_name = get_name(&link).unwrap_or_else(|_| { "unknown_name".to_string() });
     vid_name.push_str(".mp3");
-    let aud = Command::new("youtube-dl")
-        .args(["-f", "bestaudio[ext=m4a]", "-o", "-", &link])
+    let aud = Command::new("yt-dlp")
+        .args(["-f", "bestaudio[ext=m4a][filesize_approx<=50m]", "-o", "-", &link])
         .output();
     if let Ok(aud) = aud {
         if aud.stdout.is_empty() {
